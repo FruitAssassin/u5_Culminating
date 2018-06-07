9342824e056a4e48bd6cf6316d75a63db6c259ba
JoshDegazio <36270908+JoshDegazio@users.noreply.github.com>
2018-06-07 10:05:03 -0400

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,42 +32,31 @@ namespace u5_Culminating
 
     public static class Globals
     {
-
-
-        public static bool areAliensCreated = false;
-        public static bool areAliensCreated2 = false;
-        public static bool areAliensCreated3 = false;
-        public static bool areAliensCreated4 = false;
         public static bool beginfade = false;
         public static bool musicPlaying = false;
 
 
-        public static int SP1AliensCreated = 0;
-        public static int SP2AliensCreated = 0;
-        public static int SP3AliensCreated = 0;
-        public static int SP4AliensCreated = 0;
+        public static int ApplesCreated = 0;
+        public static int PineApplesCreated = 0;
+        public static int BananasCreated = 0;
+        public static int WatermelonCreated = 0;
 
+        public static ImageBrush MMBackground = new ImageBrush(new BitmapImage(new Uri(@"Images\Dojo Background.png", UriKind.Relative)));
 
-        public static ImageBrush Fake_Apple = new ImageBrush(new BitmapImage(new Uri("FakeApple.png", UriKind.Relative)));
-        public static ImageBrush Fake_Banana = new ImageBrush(new BitmapImage(new Uri("FakeBanana.png", UriKind.Relative)));
-        public static ImageBrush Fake_Pineapple = new ImageBrush(new BitmapImage(new Uri("FakePineapple.png", UriKind.Relative)));
-        public static ImageBrush Fake_Watermelon = new ImageBrush(new BitmapImage(new Uri("FakeWatermelon.png", UriKind.Relative)));
-        public static ImageBrush Fake_Bomb = new ImageBrush(new BitmapImage(new Uri("FakeBomb.png", UriKind.Relative)));
 
         public static SoundPlayer musicPlayer = new SoundPlayer();
         public static MediaPlayer effectPlayer = new MediaPlayer();
+
+
     }
 
     public partial class MainWindow : Window
     {
+        System.Windows.Threading.DispatcherTimer gameTimer = new System.Windows.Threading.DispatcherTimer();
+        Sword player;
+
 
         public GameState gameState;
-
-        Rectangle FakeApple = new Rectangle();
-        Rectangle FakeBanana = new Rectangle();
-        Rectangle FakePineapple = new Rectangle();
-        Rectangle FakeWatermelon = new Rectangle();
-        Rectangle FakeBomb = new Rectangle();
 
 
         TextBlock txt_Begin = new TextBlock();
@@ -76,68 +65,17 @@ namespace u5_Culminating
 
         public MainWindow()
         {
-
-
             InitializeComponent();
 
+            gameTimer.Tick += gameTimer_Tick;
+            gameTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);//fps
+            gameTimer.Start();
+
+            canvas_mainmenu.Background = Globals.MMBackground;
+            canvas_mainmenu.Visibility = Visibility.Visible;
             gameState = GameState.MainMenu;
-            CreateMainMenu();
-
-
 
         }
-
-        private void CreateMainMenu()
-        {
-
-
-            FakeApple.Fill = Globals.Fake_Apple; FakeApple.Height = 128; FakeApple.Width = 128; Canvas.SetTop(FakeApple, 84); Canvas.SetLeft(FakeApple, 276);
-            FakeBanana.Fill = Globals.Fake_Banana; FakeBanana.Height = 128; FakeBanana.Width = 128; Canvas.SetTop(FakeBanana, 148); Canvas.SetLeft(FakeBanana, 176);
-            FakePineapple.Fill = Globals.Fake_Pineapple; FakePineapple.Height = 128; FakePineapple.Width = 128; Canvas.SetTop(FakePineapple, 148); Canvas.SetLeft(FakePineapple, 101);
-            FakeWatermelon.Fill = Globals.Fake_Watermelon; FakeWatermelon.Height = 128; FakeWatermelon.Width = 128; Canvas.SetTop(FakeWatermelon, 148); Canvas.SetLeft(FakeWatermelon, 451);
-            FakeBomb.Fill = Globals.Fake_Bomb; FakeBomb.Height = 128; FakeBomb.Width = 128; Canvas.SetTop(FakeBomb, 148); Canvas.SetLeft(FakeBomb, 376);
-
-
-
-            canvas_mainmenu.Children.Add(FakeApple);
-            canvas_mainmenu.Children.Add(FakeBanana);
-            canvas_mainmenu.Children.Add(FakePineapple);
-            canvas_mainmenu.Children.Add(FakeWatermelon);
-            canvas_mainmenu.Children.Add(FakeBomb);
-
-        }
-
-
-        private void FadeBeginText()
-        {
-            if (gameState == GameState.MainMenu)
-            {
-                if (Globals.beginfade == false)
-                {
-                    if (txt_Begin.Opacity != 0 || txt_Begin.Opacity >= 1)
-                    {
-                        txt_Begin.Opacity = txt_Begin.Opacity - 0.025;
-                    }
-                    if (txt_Begin.Opacity <= 0)
-                    {
-                        Globals.beginfade = true;
-                    }
-                }
-                if (Globals.beginfade == true)
-                {
-                    if (txt_Begin.Opacity != 1 || txt_Begin.Opacity >= 0)
-                    {
-                        txt_Begin.Opacity = txt_Begin.Opacity + 0.025;
-                    }
-                    if (txt_Begin.Opacity >= 1)
-                    {
-                        Globals.beginfade = false;
-                    }
-                }
-            }
-        }
-
-
 
         private void MusicEvents()
         {
@@ -166,7 +104,23 @@ namespace u5_Culminating
                 }
             }
         }
+
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (gameState == GameState.GameOn)
+            {
+                if (Mouse.LeftButton == MouseButtonState.Pressed)
+                {
+                    Sword player = new Sword(canvas_battleground, this);
+                }
+            }
+        }
+
+
+        public void Click_Play(object sender, RoutedEventArgs e)
+        {
+            gameState = GameState.GameOn;
+        }
     }
 }
-        
-
