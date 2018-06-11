e43ba4018ea19a06a25441b3e5c2f746b4751386
JoshDegazio <36270908+JoshDegazio@users.noreply.github.com>
2018-06-11 14:04:13 -0400

    Add files via upload

---
+++ Sword.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace u5_Culminating
+{
+    class Sword
+    {
+        //Generate Player Variables
+        Point SwordPos = new Point();
+        private Point point;
+        public Point Point { get => point; }
+        Canvas canvas;
+        MainWindow window;
+        Rectangle SwordRectangle;
+        public Rect boundingBox { get => box; }
+        Rect box;
+        Random r = new Random(5);
+
+
+        //Create Sprites
+        ImageBrush s_Katana = new ImageBrush(new BitmapImage(new Uri(@"Images\Katana.png", UriKind.Relative)));
+
+        public Sword(Canvas c, MainWindow w)
+        {
+            //Generate Alien
+            canvas = c;
+            window = w;
+
+            SwordRectangle = new Rectangle();
+            SwordRectangle.Fill = s_Katana;
+            SwordRectangle.Height = 64;
+            SwordRectangle.Width = 128;
+            canvas.Children.Add(SwordRectangle);
+            Canvas.SetTop(SwordRectangle, point.Y);
+            Canvas.SetLeft(SwordRectangle, point.X);
+            box = new Rect(point, new Size(64, 64));
+            int rOthernumber = r.Next();
+
+        }
+
+        public void Tick()
+        {
+        }
+
+        private void Movement()
+        {
+        }
+
+        public void destroy()
+        {
+            canvas.Children.Remove(SwordRectangle);
+        }
+
+    }
+
+}
